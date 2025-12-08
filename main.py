31cd6339913b9cfaff50506909a78581b527abd1
CNicoletti <cristianonicoletti33@gmail.com>
2025-12-08 22:05:26 +0200

    Make a window using PySide6

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-# Import Libraries
+from PySide6.QtWidgets import QApplication, QWidget
+import sys
 
-from flask import Flask, request, jsonify
-import json
-import os
-import uuid
-from datetime import datetime
+app = QApplication(sys.argv)
 
+window = QWidget()
+window.setWindowTitle("My App")
+window.resize(400, 300)
+window.show()
+
+sys.exit(app.exec())
